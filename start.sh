d4a04521930fe6d0b4f1c5af13ac1b20faf33193
Brian McGehee <doct15@yahoo.com>
2020-12-02 10:58:57 -0800

    init

---
+++ start.sh
@@ -0,0 +1,3 @@
+#
+./ethdcrminer64 -ewal a4df0737ee0345271b41105e2e37a3eae471d772 -epsw x -eworker linux -epool us2.ethermine.org:4444 -allpools 1 -mode 1 -colors 1 -nofee 0 -mport 0
+#./ethdcrminer64 -ewal a4df0737ee0345271b41105e2e37a3eae471d772 -epsw x -eworker linux -epool us2.ethermine.org:4444 -allpools 1 -mode 1 -colors 1 -nofee 0
